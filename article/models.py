64a7db8d730361cff1ca23c630b297fedd6c0dd2
feri <fermesk68@gmail.com>
2021-06-17 22:18:26 +0200

    article model

--- article/models.py
+++ article/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    body =models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField()
+#add thubmnail and authors
